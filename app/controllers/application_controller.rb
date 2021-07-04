687adbd83382118e2f89fc422b0cab840ff6313e
suzuharu0920 <sz.haruto0920@icloud.com>
2021-07-04 17:49:15 +0900

    新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'suzuki' && password == '0920'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :first_name_kana, :last_name_kana, :first_name, :last_name,])
   end
 end
