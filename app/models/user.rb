d01629318f5f14881184f0d63e4900fdfd45f62f
suzuharu0920 <sz.haruto0920@icloud.com>
2021-07-05 21:31:47 +0900

    無駄なものの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_many :purchases
   
   validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
 end
