1be6961a3835fe6e786bbe0137aa1378236e96c6
suzuharu0920 <sz.haruto0920@icloud.com>
2021-07-06 14:22:27 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :purchases
+  # has_many :items
+  # has_many :purchases
   
-  validates :nickname,                  presence: true
-  validates :email,                     presence: true
-  validates :password,                  presence: true
-  validates :password_confirmation,     presence: true
-  validates :first_name,                presence: true
-  validates :last_name,                 presence: true
-  validates :first_name_kana,           presence: true
-  validates :last_name_kana,            presence: true
-  validates :birthday,                  presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end
