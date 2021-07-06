6bb103051fff3986f065396fb91b45b56065ca26
suzuharu0920 <sz.haruto0920@icloud.com>
2021-07-06 12:11:53 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
   
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+  validates :nickname,                  presence: true
+  validates :email,                     presence: true
+  validates :password,                  presence: true
+  validates :password_confirmation,     presence: true
+  validates :first_name,                presence: true
+  validates :last_name,                 presence: true
+  validates :first_name_kana,           presence: true
+  validates :last_name_kana,            presence: true
+  validates :birthday,                  presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,16 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
+    it "emailが他のユーザーと重複していると登録できないこと" do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it "emailには「@」を含む必要があること" do
+      @user.email = 'hoge.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email is invalid"
+    end
     it "passwordが空では登録できない" do
       @user.password = ''
       @user.valid?
@@ -25,8 +35,33 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
+    it "パスワードは、6文字以上での入力が必須であること" do
+      @user.password = 'aaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+    it "半角英語のみでは登録できない" do
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+    it "半角数字のみでは登録できない" do
+      @user.password_confirmation = '00'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+    it "全角では登録できないこと" do
+      @user.password_confirmation = '００'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
     it "first_nameが空では登録できない" do
       @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include
+    end
+    it "first_nameが漢字・平仮名・片仮名以外だと登録できない" do
+      @user.first_name = 'a'
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
@@ -35,13 +70,28 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
+    it "last_nameが漢字・平仮名・片仮名以外だと登録できない" do
+      @user.last_name = 'a'
+      @user.valid?
+      expect(@user.errors.full_messages).to include
+    end
     it "first_name_kanaが空では登録できない" do
       @user.first_name_kana = ''
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
+    it "first_name_kanaが片仮名以外だと登録できない" do
+      @user.first_name_kana = 'あ, a'
+      @user.valid?
+      expect(@user.errors.full_messages).to include
+    end
     it "last_name_kanaが空では登録できない" do
       @user.last_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include
+    end
+    it "last_name_kanaが片仮名以外だと登録できない" do
+      @user.last_name_kana = 'あ, a'
       @user.valid?
       expect(@user.errors.full_messages).to include
     end
