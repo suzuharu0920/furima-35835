2c53fb69eba35f38a7314a21a526a60aaeac7686
suzuharu0920 <sz.haruto0920@icloud.com>
2021-07-18 16:27:34 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-  
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :destroy]
+
   def index
     @items = Item.includes(:user).order("created_at ASC")
   end
@@ -16,6 +17,21 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
