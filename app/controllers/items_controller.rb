d75d866efdc08d74f91eec2405d771602f815622
suzuharu0920 <sz.haruto0920@icloud.com>
2021-07-18 17:55:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy]
+  before_action :set_item, only: [:show]#:destroy
 
   def index
     @items = Item.includes(:user).order("created_at ASC")
@@ -22,13 +22,13 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def destroy
+  # def destroy
     # if @item.destroy
     #   redirect_to root_path
     # else
     #   redirect_to root_path
     # end
-  end
+  # end
 
   def set_item
     @item = Item.find(params[:id])
