2bb0badc221b094fdb8236f31d0864a8261b7c65
suzuharu0920 <sz.haruto0920@icloud.com>
2021-07-19 12:52:32 +0900

    コメントアウトしていた部分の復元

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,13 +33,13 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # if @item.destroy
-  #   redirect_to root_path
-  # else
-  #   redirect_to root_path
-  # end
-  # end
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   private
 
